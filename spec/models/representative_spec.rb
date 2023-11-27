705f971e00f1b4b41e233196bc3f795f8ac93108
Winson Tsang Wan <winsontsangwan@berkeley.edu>
2023-11-27 10:20:22 +0000

    fix rep model rspec test and delete countyFipsCode line

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable RSpec/VerifiedDoubleReference
+
 require 'rails_helper'
 
 RSpec.describe Representative, type: :model do
@@ -11,14 +13,28 @@ RSpec.describe Representative, type: :model do
 
     it 'does not create duplicate representatives' do
       # Mock representative data from the API with a duplicate name
-      official = instance_double(official, name: 'John Doe')
-      office = instance_double(office, name: 'Mayor', division_id: 'ocdid_456', official_indices: [0])
-      rep_info = instance_double(rep_info, officials: [official], offices: [office])
+      official = instance_double('official', name: 'John Doe', address: nil, party: nil, photo_url: nil)
+      office = instance_double('office', name: 'Mayor', division_id: 'ocdid_456', official_indices: [0])
+      rep_info = instance_double('rep_info', officials: [official], offices: [office])
       # Call the method
       described_class.civic_api_to_representative_params(rep_info)
 
       # Check that only one representative with the name 'John Doe' exists in the database
       expect(described_class.where(name: 'John Doe').count).to eq(1)
     end
+
+    it 'creates a representatives' do
+      # Mock representative data from the API with a duplicate name
+      official = instance_double('official', name: 'Phil Er', address: nil, party: nil, photo_url: nil)
+      office = instance_double('office', name: 'Mayor', division_id: 'ocdid_457', official_indices: [0])
+      rep_info = instance_double('rep_info', officials: [official], offices: [office])
+      # Call the method
+      described_class.civic_api_to_representative_params(rep_info)
+
+      # Check that only one representative with the name 'Phil Er' exists in the database
+      expect(described_class.where(name: 'Phil Er').count).to eq(1)
+    end
   end
 end
+
+# rubocop:enable RSpec/VerifiedDoubleReference
