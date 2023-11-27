a3289ea81500ff2e6782a7defda84fb4a4864f2a
Winson Tsang Wan <winsontsangwan@berkeley.edu>
2023-11-27 21:49:54 +0000

    separate new columns in representative into new migration file

--- db/migrate/20200728065604_add_columns_to_representatives.rb
+++ db/migrate/20200728065604_add_columns_to_representatives.rb
@@ -4,8 +4,5 @@ class AddColumnsToRepresentatives < ActiveRecord::Migration[5.2]
   def change
     add_column :representatives, :ocdid, :string
     add_column :representatives, :title, :string
-    add_column :representatives, :party, :string
-    add_column :representatives, :address, :json
-    add_column :representatives, :photo_url, :string
   end
 end
