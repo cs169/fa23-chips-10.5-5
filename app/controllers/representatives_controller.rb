c5307445f0924f37c01aedcd05421baaf92d9205
Jason Lee <leejason2025@berkeley.edu>
2023-11-27 22:12:06 +0000

    iter 1 part 3 fixed

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -2,10 +2,6 @@
 
 class RepresentativesController < ApplicationController
   def index
-    if params[:county]
-      @representatives = Representative.where(county: params[:county])
-    else
-      @representatives = Representative.all
-    end
+    @representatives = Representative.all
   end
 end
