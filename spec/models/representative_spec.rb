7f563bc8309ac0b13dbc0037d29571ee4864abcf
Lawrence Lam <lawrencelam@berkeley.edu>
2023-12-03 09:51:35 +0000

    tried cucumber tests

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -26,13 +26,12 @@ RSpec.describe Representative, type: :model do
     it 'creates a representatives' do
       # Mock representative data from the API with a duplicate name
       official = instance_double('official', name: 'Phil Er', address: nil, party: nil, photo_url: nil)
-
       office = instance_double('office', name: 'Mayor', division_id: 'ocdid_457', official_indices: [0])
       rep_info = instance_double('rep_info', officials: [official], offices: [office])
       # Call the method
       described_class.civic_api_to_representative_params(rep_info)
 
-      # Check that only one representative with the name 'John Doe' exists in the database
+      # Check that only one representative with the name 'Phil Er' exists in the database
       expect(described_class.where(name: 'Phil Er').count).to eq(1)
     end
   end
